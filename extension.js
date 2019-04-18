063e3d2b1584bcb870721161374fcc55ec457251
George Plotnikov <accembler@gmail.com>
2019-04-18 23:19:57 +0300

    added md to xml render function
    use csl-generate-xml palette command

--- extension.js
+++ extension.js
@@ -1,6 +1,41 @@
 const cuMarkdown = require('markdown-it-church-slavonic');
+const vscode = require('vscode');
+var markdownit = require('markdown-it');
 
-exports.activate = function activate() {
+require('supports-color');
+
+const fs = require("fs");
+
+/**
+ * @param {vscode.ExtensionContext} context
+ */
+function activate(context) {
+
+    let disposable = vscode.commands.registerCommand('csl.generateXml', function () {  
+        let currentFile =  vscode.window.activeTextEditor.document.uri.fsPath;
+        var xmlRendrer = markdownit().use(cuMarkdown, { renderer: 'xml' });
+        fs.readFile(currentFile, 'utf8', function(err, data) {
+            if (err) throw err;
+            let xml = xmlRendrer.render(data);
+            let newFileName = currentFile.substr(0, currentFile.lastIndexOf('.')) + '.xml';
+            fs.writeFile(newFileName, xml, err => {
+                if (err) {
+                    return vscode.window.showErrorMessage(
+                      "Failed to create boilerplate file!"
+                    );
+                } else {
+                    vscode.window.showInformationMessage('File sucessfully created');
+                    vscode.workspace.openTextDocument(newFileName).then(doc => {
+                        vscode.window.showTextDocument(doc);
+                     });
+                }
+            });
+
+          });
+    });
+
+    context.subscriptions.push(disposable);
+
     return {
         extendMarkdownIt(md) {
             return md.use(cuMarkdown);
@@ -8,4 +43,5 @@ exports.activate = function activate() {
     }
 }
 
+exports.activate = activate;
 exports.deactivate = function deactivate() {};
